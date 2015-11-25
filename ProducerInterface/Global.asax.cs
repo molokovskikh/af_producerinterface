b0f92769d09b6978cdc3fe320dd2f558095540f1
U-ANALIT\alegusov <alegusov@MSK-PRG2.adc.analit.net>
2015-11-25 15:17:28 +0300

    Change Logo .etc

--- ProducerInterface/Global.asax.cs
+++ ProducerInterface/Global.asax.cs
@@ -11,12 +11,12 @@ namespace ProducerInterface
 	public class MvcApplication : System.Web.HttpApplication
 	{
 		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			Framework.Initialize(this);
+		{        
+                AreaRegistration.RegisterAllAreas();
+                WebApiConfig.Register(GlobalConfiguration.Configuration);
+                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+                RouteConfig.RegisterRoutes(RouteTable.Routes);
+                Framework.Initialize(this);         
 		}
 	}
 }
