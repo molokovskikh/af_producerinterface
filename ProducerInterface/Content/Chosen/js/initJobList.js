76f998a60c6fcd4145c9e838b7ea4e505c49cc3d
Максименко Герман <g.maksimenko@analit.net>
2015-12-31 14:07:25 +0300

    Правки 14:07

--- ProducerInterface/Content/Chosen/js/initJobList.js
+++ ProducerInterface/Content/Chosen/js/initJobList.js
@@ -9,32 +9,30 @@ function ajaxCall() {
 		var el = $(this);
 		var url = el.data('url');
 		$.getJSON(url, null, function(data) {
-			if (data.status == 2)
-				el.html('<a href="' + data.url + '">Отчёт готов</a>');
-				el.removeClass('processed');
+			switch (data.status) {
+				case 0:
+					el.text('Не запускался');
+					break;
+				case 1:
+			  	el.text('Отчёт готовится');
+			  	break;
+				case 2:
+					el.html('<a href="' + data.url + '">Отчёт готов</a>');
+					el.removeClass('processed');
+					break;
+				case 3:
+					el.text('Нет данных для построения отчёта');
+					break;
+				case 4:
+					el.text('В процессе подготовки произошла ошибка');
+					break;
+				default:
+					el.text('Неизвестный статус');
+			}		
 		});
 	});
 };
 
 
-//switch (data.status) {
-//	case 0:
-//		el.text('Не запускался');
-//		break;
-//	case 1:
-//  	el.text('Отчёт готовится');
-//  	break;
-//	case 2:
-//		el.html('<a href="' + data.url + '">Отчёт готов</a>');
-//		el.removeClass('processed');
-//		break;
-//	case 3:
-//		el.text('Нет данных для построения отчёта');
-//		break;
-//	case 4:
-//		el.text('В процессе подготовки произошла ошибка');
-//		break;
-//	default:
-//		el.text('Неизвестный статус');
-//}
+
 
