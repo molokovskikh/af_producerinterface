4640dd040224635801dbd35d99511ab4bcd972ad
Legusov Aleksandr <alegusov@analit.net>
2016-02-04 19:29:42 +0300

    Регистрация

--- ProducerInterface/Controllers/DrugController.cs
+++ ProducerInterface/Controllers/DrugController.cs
@@ -24,7 +24,7 @@ namespace ProducerInterface.Controllers
 			//  TODO: берётся у юзера            
 			try {
 				userId = CurrentUser.Id;
-				producerId = (long)CurrentUser.CompanyId;
+				producerId = (long)CurrentUser.AccountCompany.ProducerId;
 			}
 			catch {
 				// Ignore
