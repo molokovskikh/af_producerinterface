6a67b8d85ab3f05073d6db58a031287a5ddbc5d8
Legusov Aleksandr <alegusov@analit.net>
2016-02-12 16:28:00 +0300

    12 02 2016

--- ProducerInterface/Scripts/WorkPlace/DatePicker.js
+++ ProducerInterface/Scripts/WorkPlace/DatePicker.js
@@ -23,3 +23,42 @@
         }
     }).data('datepicker');
 });
+
+function UpKey()
+{
+    var characters = 1000;
+    $("#counter").append(characters);
+    $("#annotation").keyup(function () {
+        if ($(this).val().length > characters) {
+            $(this).val($(this).val().substr(0, characters));
+        }
+        var remaining = characters - $(this).val().length;
+        document.getElementById('#annotation').innerHTML = remaining;
+     //   $("#counter").innerHTML = remaining.toString();     
+        if (remaining <= 999) {
+            $("#counter").css("color", "red");
+        }
+        else {
+            $("#counter").css("color", "black");
+        }
+    });
+}
+
+$(function () {
+    var characters = 1000;
+    $("#counter").append(characters);
+    $("#annotation").keyup(function () {
+        if ($(this).val().length > characters) {
+            $(this).val($(this).val().substr(0, characters));
+        }
+        var remaining = characters - $(this).val().length;
+        $("#counter").html(remaining);
+        alert(remaining);
+        if (remaining <= 999) {
+            $("#counter").css("color", "red");
+        }
+        else {
+            $("#counter").css("color", "black");
+        }
+    });
+});
