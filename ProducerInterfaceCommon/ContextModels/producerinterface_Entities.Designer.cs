aacbbd80375518f09213b939636bcecee21c0e63
Максименко Герман <g.maksimenko@analit.net>
2016-02-16 20:57:07 +0300

    Правки по списку отчётов и рейтингу товаров

--- ProducerInterfaceCommon/ContextModels/producerinterface_Entities.Designer.cs
+++ ProducerInterfaceCommon/ContextModels/producerinterface_Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\alegusov\projects\producerinterface\ProducerInterfaceCommon\ContextModels\producerinterface_Entities.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\maksimenko\projects\producerinterface\ProducerInterfaceCommon\ContextModels\producerinterface_Entities.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
--- ProducerInterfaceCommon/Heap/NamesHelper.cs
+++ ProducerInterfaceCommon/Heap/NamesHelper.cs
@@ -7,7 +7,7 @@ namespace ProducerInterfaceCommon.Heap
 {
 	public class NamesHelper
 	{
-		
+
 		private ProducerInterfaceCommon.ContextModels.producerinterface_Entities _cntx;
 
 		private long _userId;
@@ -30,29 +30,29 @@ namespace ProducerInterfaceCommon.Heap
 			//results.Add(new OptionElement() { Text = "r.kvasov@analit.net", Value = "r.kvasov@analit.net" });
 
 			// TODO: до переноса в ProducerInterface пользователя может и не быть, далее он обязан быть
-			var u =_cntx.Account.SingleOrDefault(x => x.Id == _userId);
+			var u = _cntx.Account.SingleOrDefault(x => x.Id == _userId);
 			if (u != null)
 				results.Add(new OptionElement() { Text = u.Login, Value = u.Login });
 
 			return results;
 		}
-     
-        // для UI ???
-        public string GetMailOkReportSubject()
+
+		// для UI ???
+		public string GetMailOkReportSubject()
 		{
 			// TODO: до переноса в ProducerInterface пользователя может и не быть, далее он обязан быть
 			var result = "Отчет пользователя *** производителя *** (будет указано после переноса в ProducerInterface)";
-			var u =_cntx.Account.SingleOrDefault(x => x.Id == _userId);
-            if (u != null)
-            {
-                var X = _cntx.producernames.Where(xxx => xxx.ProducerId == u.AccountCompany.ProducerId).FirstOrDefault();
-                string companyName = "";
-                if (X != null && X.ProducerId != 0) { companyName = X.ProducerName; }
-                else { companyName = u.AccountCompany.Name; }
+			var u = _cntx.Account.SingleOrDefault(x => x.Id == _userId);
+			if (u != null)
+			{
+				var X = _cntx.producernames.Where(xxx => xxx.ProducerId == u.AccountCompany.ProducerId).FirstOrDefault();
+				string companyName = "";
+				if (X != null && X.ProducerId != 0) { companyName = X.ProducerName; }
+				else { companyName = u.AccountCompany.Name; }
 
-                
-                result = $"Отчет пользователя {u.Name} {u.Login} производителя {companyName}";
-            }
+
+				result = $"Отчет пользователя {u.Name} {u.Login} производителя {companyName}";
+			}
 			return result;
 		}
 
@@ -78,73 +78,81 @@ namespace ProducerInterfaceCommon.Heap
 			return results;
 		}
 
-        public List<OptionElement> GetCatalogListPromotion()
-        {
-            var producerId = _cntx.Account.Single(x => x.Id == _userId).AccountCompany.ProducerId;
-            var NewListDrug = _cntx.promotionToDrug.Where(xxx => _cntx.promotions.Where(xx => xx.ProducerId == producerId)              
-                .Select(xx => xx.Id).ToList().Contains(xxx.PromotionId)).Select(x=>x.DrugId).ToList();      
-            var results = _cntx.assortment.Where(xxx => NewListDrug.Contains(xxx.CatalogId))
-                .GroupBy(x => x.CatalogId).Select(x => x.FirstOrDefault())
-                .Select(xxx => new OptionElement { Text = xxx.CatalogName, Value = xxx.CatalogId.ToString() })               
-                .ToList();         
-            return results;
-        }
+		public List<OptionElement> GetCatalogListPromotion()
+		{
+			var producerId = _cntx.Account.Single(x => x.Id == _userId).AccountCompany.ProducerId;
+			var NewListDrug = _cntx.promotionToDrug.Where(xxx => _cntx.promotions.Where(xx => xx.ProducerId == producerId)
+					.Select(xx => xx.Id).ToList().Contains(xxx.PromotionId)).Select(x => x.DrugId).ToList();
+			var results = _cntx.assortment.Where(xxx => NewListDrug.Contains(xxx.CatalogId))
+					.GroupBy(x => x.CatalogId).Select(x => x.FirstOrDefault())
+					.Select(xxx => new OptionElement { Text = xxx.CatalogName, Value = xxx.CatalogId.ToString() })
+					.ToList();
+			return results;
+		}
 
-        public List<OptionElement> GetDrugList(List<long> SelectedDrugs)
-        {
-            if (SelectedDrugs == null || SelectedDrugs.Count() == 0)
-                return new List<OptionElement>();
+		public List<OptionElement> GetDrugList(List<long> SelectedDrugs)
+		{
+			if (SelectedDrugs == null || SelectedDrugs.Count() == 0)
+				return new List<OptionElement>();
 
-            var ListDrugsNoConvert = _cntx.drugfamilynames.Where(x => SelectedDrugs.Contains(x.FamilyId)).ToList();
-            var ListSelectGrugs = ListDrugsNoConvert.Select(xxx => new OptionElement { Text = xxx.FamilyName, Value = xxx.FamilyId.ToString() }).ToList();
-            return ListSelectGrugs;
-        }
+			var ListDrugsNoConvert = _cntx.drugfamilynames.Where(x => SelectedDrugs.Contains(x.FamilyId)).ToList();
+			var ListSelectGrugs = ListDrugsNoConvert.Select(xxx => new OptionElement { Text = xxx.FamilyName, Value = xxx.FamilyId.ToString() }).ToList();
+			return ListSelectGrugs;
+		}
 
-        public List<OptionElement> GetSearchCatalogFamalyName(string NameDrug)
-        {
-            var result = _cntx.drugfamilynames.Where(xxx => xxx.FamilyName.Contains(NameDrug)).Take(10).ToList().Select(xxx => new OptionElement { Value = xxx.FamilyId.ToString(), Text = xxx.FamilyName }).ToList();
-            return result;
-        }
+		public List<OptionElement> GetSearchCatalogFamalyName(string NameDrug)
+		{
+			var result = _cntx.drugfamilynames.Where(xxx => xxx.FamilyName.Contains(NameDrug)).Take(10).ToList().Select(xxx => new OptionElement { Value = xxx.FamilyId.ToString(), Text = xxx.FamilyName }).ToList();
+			return result;
+		}
 
-        // для UI
-        public List<OptionElement> GetSupplierList(List<decimal> regionList)
+		// для UI
+		public List<OptionElement> GetSupplierList(List<decimal> regionList)
 		{
 			// если регионы не указаны - возвращаем пустой лист
 			if (regionList == null)
 				return new List<OptionElement>();
 
-			var regionMask = regionList.Select(x => (ulong)x).Aggregate((y, z) => y | z);
-			var suppliers = _cntx.suppliernames.ToList();
-			var results = suppliers
-				.Where(x => ((ulong)x.HomeRegion.Value & regionMask) > 0)
+			var suppliersInRegions = _cntx.supplierregions.Where(x => regionList.Contains(x.RegionCode.Value)).Select(x => x.SupplierId).ToList();
+			var results = _cntx.suppliernames
+				.Where(x => suppliersInRegions.Contains(x.SupplierId))
 				.OrderBy(x => x.SupplierName)
-				.Select(x => new OptionElement { Value = x.SupplierId.ToString(), Text = x.SupplierName }).ToList();
+				.Select(x => new OptionElement { Value = x.SupplierId.ToString(), Text = x.SupplierName })
+				.ToList();
+
+			//var regionMask = regionList.Select(x => (ulong)x).Aggregate((y, z) => y | z);
+			//var suppliers = _cntx.suppliernames.ToList();
+			//var results = suppliers
+			//	.Where(x => ((ulong)x.HomeRegion.Value & regionMask) > 0)
+			//	.OrderBy(x => x.SupplierName)
+			//	.Select(x => new OptionElement { Value = x.SupplierId.ToString(), Text = x.SupplierName }).ToList();
 			return results;
 		}
-        public List<long> GetPromotionRegions(ulong ?RegionMask)
-        {
-            if (RegionMask == null)
-            {
-                return new List<long>();
-            }      
 
-            var LisrRegions = _cntx.regionnames.OrderBy(x => x.RegionName).ToList();
+		public List<long> GetPromotionRegions(ulong? RegionMask)
+		{
+			if (RegionMask == null)
+			{
+				return new List<long>();
+			}
 
-            var results = LisrRegions
-            .Where(x =>    
-            ((ulong)x.RegionCode & RegionMask) > 0)
-            .OrderBy(x => x.RegionCode)
-            .Select(x => (long) x.RegionCode).ToList();
-            return results;
-        }
+			var LisrRegions = _cntx.regionnames.OrderBy(x => x.RegionName).ToList();
 
-        public List<OptionElement> GetProducerList()
-        {
-            var X = _cntx.producernames.ToList().Select(xxx => new OptionElement { Text = xxx.ProducerName, Value = xxx.ProducerId.ToString() }).ToList();
-            var Y = new List<OptionElement>() { new OptionElement { Text = "", Value = "" } };
-            Y.AddRange(X);
+			var results = LisrRegions
+			.Where(x =>
+			((ulong)x.RegionCode & RegionMask) > 0)
+			.OrderBy(x => x.RegionCode)
+			.Select(x => (long)x.RegionCode).ToList();
+			return results;
+		}
 
-            return Y;
-        }
-    }
+		public List<OptionElement> GetProducerList()
+		{
+			var X = _cntx.producernames.ToList().Select(xxx => new OptionElement { Text = xxx.ProducerName, Value = xxx.ProducerId.ToString() }).ToList();
+			var Y = new List<OptionElement>() { new OptionElement { Text = "", Value = "" } };
+			Y.AddRange(X);
+
+			return Y;
+		}
+	}
 }
