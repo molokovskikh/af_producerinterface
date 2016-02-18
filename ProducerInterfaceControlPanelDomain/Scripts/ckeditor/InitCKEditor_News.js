e6820c42cb75a2d46d7031d8092393a757a498c2
Legusov Aleksandr <alegusov@analit.net>
2016-02-18 16:46:06 +0300

    Правки интерфейса новостей, добавлена возможно предпросмотра новости в отдельном окне до публикации из панели управления

---
+++ ProducerInterfaceControlPanelDomain/Scripts/ckeditor/InitCKEditor_News.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    CKEDITOR.replace('Description',
+        {
+            'filebrowserBrowseUrl': '/MediaFiles/Index/',
+            'filebrowserImageBrowseUrl': '/MediaFiles/Index/',
+            'filebrowserUploadUrl': '/MediaFiles/SaveFile/',
+            'filebrowserImageUploadUrl': '/MediaFiles/SaveFile/',
+            'height': '35em'
+        });
+});
+function TargetBlackPreviewNews() {
+    var Param1 = $('#Name').val();
+    var ParamDescription = CKEDITOR.instances.Description.getData();
+    //  var Param2 = $('#Description').val();
+    window.open('http://localhost:53367/News/Preview' + "?Name=" + Param1 + "&Description=" + ParamDescription, '_blank');
+}
