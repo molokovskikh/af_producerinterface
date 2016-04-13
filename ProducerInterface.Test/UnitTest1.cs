63baf0ab4b7e43126953bc94c4f17e3ab778f76c
Максименко Герман <g.maksimenko@analit.net>
2016-04-13 22:06:09 +0300

    Доб. тест

---
+++ ProducerInterface.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ProducerInterface.Test
+{
+	[TestFixture]
+	public class UnitTest1
+	{
+		[Test]
+		public void TestMethod1()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
