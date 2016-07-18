dc835b7e1e54314f08e360248fe7fae42313b340
Квасов Роман <r.kvasov@analit.net>
2016-07-18 16:29:06 +0300

    Улучшил протоколирование

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-iis: iisexpress /systray:false /path:`cygpath -wa ProducerInterface` | iconv -f cp866 -t utf-8
-iis: iisexpress /port:8081 /systray:false /path:`cygpath -wa ProducerInterfaceControlPanelDomain` | iconv -f cp866 -t utf-8
+iis: iisexpress /systray:false /path:`cygpath -wa client/app` | iconv -f cp866 -t utf-8
+iis: iisexpress /port:8081 /systray:false /path:`cygpath -wa adm/app` | iconv -f cp866 -t utf-8
--- adm/app/Global.asax.cs
+++ adm/app/Global.asax.cs
@@ -1,23 +1,52 @@
 ﻿using System;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using log4net;
 using log4net.Config;
+using log4net.ObjectRenderer;
+using MySql.Data.MySqlClient;
 
 namespace ProducerInterfaceControlPanelDomain
 {
+		public class ExceptionRenderer : IObjectRenderer
+	{
+		public void RenderObject(RendererMap rendererMap, object obj, TextWriter writer)
+		{
+			var ex = obj as DbEntityValidationException;
+			if (ex != null) {
+				writer.Write(ex);
+				writer.WriteLine();
+				foreach (var childEx in ex.EntityValidationErrors) {
+					writer.Write(childEx);
+					writer.WriteLine();
+				}
+			}
+		}
+	}
+
 	public class MvcApplication : HttpApplication
 	{
+		static ILog Log = LogManager.GetLogger(typeof(MvcApplication));
+
 		protected void Application_Start()
 		{
-			ViewEngines.Engines.Add(new ProducerInterfaceCommon.Heap.MyViewEngine());
-			AreaRegistration.RegisterAllAreas();
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			XmlConfigurator.Configure();
+			try {
+				ViewEngines.Engines.Add(new ProducerInterfaceCommon.Heap.MyViewEngine());
+				AreaRegistration.RegisterAllAreas();
+				FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+				RouteConfig.RegisterRoutes(RouteTable.Routes);
+				BundleConfig.RegisterBundles(BundleTable.Bundles);
+				XmlConfigurator.Configure();
+				Log.Logger.Repository.RendererMap.Put(typeof(Exception), new ExceptionRenderer());
+			} catch(Exception e) {
+				Log.Error("Ошибка при инициализации приложения", e);
+				throw;
+			}
 		}
 
 		protected void Application_Error(object sender, EventArgs e)
@@ -25,14 +54,8 @@ namespace ProducerInterfaceControlPanelDomain
 			if (HttpContext.Current.IsCustomErrorEnabled)
 			{
 				var ex = Server.GetLastError();
-				ILog logger = LogManager.GetLogger(GetType());
-				logger.Error(ex.Message, ex);
-				if (Response.StatusCode != 404)
-				{
-
-				}
+				Log.Error(ex.Message, ex);
 				ErrorMessage("При выполнении запроса произошла непредвиденная ошибка");
-				//Response.Redirect("~/Home/Index");
 			}
 		}
 
