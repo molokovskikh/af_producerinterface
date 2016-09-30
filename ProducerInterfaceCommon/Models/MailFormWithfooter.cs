204e9015c07b546305dc3e165c695c7f6f4304c8
yakimenko <ayakimenko@analit.net>
2016-09-30 14:49:50 +0300

    i52909 Форматирование письма 'изменения в промоакции', тэг 'Break'

---
+++ ProducerInterfaceCommon/Models/MailFormWithfooter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProducerInterfaceCommon.Models
+{
+	public class MailFormWithFooter
+	{
+		public virtual int Id { get; set; }
+		public virtual string Subject { get; set; }
+		public virtual string Body { get; set; }
+		public virtual string Footer { get; set; }
+		public virtual bool IsBodyHtml { get; set; }
+		public virtual string Description { get; set; }
+		public virtual string Header { get; set; }
+	}
+}
